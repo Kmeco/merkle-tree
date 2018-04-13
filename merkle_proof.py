8431797f5702023bd286809af085fd21e919e4e1
Jakub Kmec <jakub.kmec@berkeley.edu>
2018-04-13 14:17:07 -0700

    all tests passed

--- merkle_proof.py
+++ merkle_proof.py
@@ -11,13 +11,24 @@ def merkle_proof(tx, merkle_tree):
 
     Return this data as a list; remember that order matters!
     """
-    tx_list = []
-    for n in merkle_tree.nodes:
-        if (tx in n.leaves):
-
-
-
-    return tx_list
+    proof_list = []
+    current_node = merkle_tree._root
+    tx_index = merkle_tree.leaves.index(tx)
+    upper_bound = len(merkle_tree.leaves)
+    lower_bound = 0
+    while upper_bound - lower_bound > 1:
+        mid = (upper_bound + lower_bound) // 2
+        if tx_index < mid:
+            proof_list.append(Node('r', current_node._right.data if type(
+                current_node._right) != str else current_node._right))
+            current_node = current_node._left
+            upper_bound = mid
+        else:
+            proof_list.append(Node('l', current_node._left.data if type(
+                current_node._left) != str else current_node._left))
+            current_node = current_node._right
+            lower_bound = mid
+    return proof_list
 
 
 
@@ -28,8 +39,13 @@ def verify_proof(tx, merkle_proof):
     that the correct block header can be retrieved by properly hashing the tx
     along with every other piece of data in the proof in the correct order
     """
-    merkle_proof = [tx] + merkle_proof
-    return concat_and_hash_list(merkle_proof)
-
-
+    # merkle_proof = ([node.tx for node in merkle_proof])[::-1]
+    # return concat_and_hash_list(merkle_proof)
+    tx_list = [tx] + merkle_proof[::-1]
+    while len(tx_list) > 1:
+        if tx_list[1].direction == 'r':
+            tx_list.insert(0, hash_data(tx_list.pop(0) + tx_list.pop(0).tx))
+        else:
+            tx_list.insert(0, hash_data(tx_list.pop(1).tx + tx_list.pop(0)))
+    return tx_list[0]
     
